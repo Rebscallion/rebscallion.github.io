555e0f2593e2d5a6c86c132b01f7999bfcada486
Rebscallion <rebekah.foley.read@gmail.com>
2022-09-07 20:18:20 +1200

    tried to fix breakpoints

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 console.log('hello');
+
+const navBttn = document.getElementById('hamburger');
+const closeBttn = document.getElementById('exit');
+
+navBttn.onclick = () => {
+    let navLinks = document.getElementById('site-nav');
+    navLinks.classList.toggle("open");
+};
+
+closeBttn.onclick = () => {
+    let navLinks = document.getElementById('site-nav');
+    navLinks.classList.toggle("open");
+};
